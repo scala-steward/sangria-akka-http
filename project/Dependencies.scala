01c90cd658770dfd0a7c291e61700e8667e3f72d
Yann Simon <yann.simon@commercetools.com>
2022-08-18 18:19:34 +0200

    update circe

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,8 @@
 object Dependencies {
   val akkaVersion = "2.6.19"
   val akkaHttpVersion = "10.2.9"
-  val circeVersion = "0.14.1"
+  val circeVersion = "0.14.2"
+  val circeOpticsVersion = "0.14.1"
   object SangriaVersion {
     val sangria = "2.1.6"
     val sangriaCirce = "1.3.2"
