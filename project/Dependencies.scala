68c8f8e9a231637665e4d7c4592035b5ae3e71a5
Yann Simon <yann.simon@commercetools.com>
2022-08-18 18:42:23 +0200

    update sangria

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   val circeVersion = "0.14.2"
   val circeOpticsVersion = "0.14.1"
   object SangriaVersion {
-    val sangria = "2.1.6"
+    val sangria = "3.2.0"
     val sangriaCirce = "1.3.2"
     val sangriaSlowlog = "2.0.4"
   }
